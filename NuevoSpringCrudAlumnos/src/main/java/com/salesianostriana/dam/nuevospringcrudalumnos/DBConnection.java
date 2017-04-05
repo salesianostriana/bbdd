7e7be10b96264f2bdc5b599ad636c61f536eadb1
salesianostriana <lmlopez@salesianos-triana.com>
2017-04-05 08:56:38 +0200

    DBConnection como Bean

--- NuevoSpringCrudAlumnos/src/main/java/com/salesianostriana/dam/nuevospringcrudalumnos/DBConnection.java
+++ NuevoSpringCrudAlumnos/src/main/java/com/salesianostriana/dam/nuevospringcrudalumnos/DBConnection.java
@@ -4,7 +4,41 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class DBConnection {
-	
-	public DBConnection() {} 
 
+	private  Connection instance = null;
+	
+	
+	public  Connection getConnection() 
+			throws ClassNotFoundException, SQLException {
+		if (instance == null) {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String url = "jdbc:oracle:thin:MRAMALLO/1@localhost:1521/XE";
+			instance = DriverManager.getConnection(url);			
+		}
+		return instance;
+	}
+	
+	public DBConnection() throws ClassNotFoundException, SQLException {
+		this.getConnection();
+	}
+	
+	public  Statement getStatement() throws ClassNotFoundException, SQLException {
+		return getConnection().createStatement();
+	}
+		
+	public  ResultSet getResultSet(String sql) throws ClassNotFoundException, SQLException {
+		return getStatement().executeQuery(sql);
+	}
+	
+	public  PreparedStatement getPreparedStatement(String sql) throws ClassNotFoundException, SQLException {
+		return getConnection().prepareStatement(sql);
+	}
+	
+	public  void close() throws SQLException {
+		instance.close();
+		instance = null;
+	}
+	
+
+	
 }
